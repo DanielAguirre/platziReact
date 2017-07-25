6bb95448dc5befcf1bbe8f4c7a9ae538ecc944c3
Daniel Aguirre <danter1@gmail.com>
2017-07-25 17:39:59 -0500

    Fix syntac errors in api.js

--- api.js
+++ api.js
@@ -1,32 +1,33 @@
 import fetch from 'isomorphic-fetch';
 
-const baserUrl = 'https://jsonplaceholder.typicode.com';
+const baseUrl = 'https://jsonplaceholder.typicode.com';
 
 const api = {
   posts: {
-    async getList(page => 1) => {
+    async getList(page = 1) {
       const response = await fetch(`${baseUrl}/posts?_page`);
       const data = await response.json();
       return data;
     },
-    async getSingle(id = 1) => {
-      const response = await fetch(`${seUrl}/posts/${id}`);
+    async getSingle(id = 1) {
+      const response = await fetch(`${baseUrl}/posts/${id}`);
       const data = await resposne.json();
+      console.log({data});
      return data;
     },
-    async getComment(id = 1) => {
-      const response = await fetch(`${seUrl}/posts/${id}/comments`);
-      const data = await resposne.json();
+    async getComments(id = 1) {
+      const response = await fetch(`${baseUrl}/posts/${id}/comments`);
+      const data = await response.json();
       return data;
     }
   },
   users: {
     async getSingle(id = 1){
-      const response = await fetch(`${seUrl}/users/${id}`);
-      const data = await resposne.json();
+      const response = await fetch(`${baseUrl}/users/${id}`);
+      const data = await response.json();
       return data;
     }
   }
 };
 
-exports default api;
+export default api;
