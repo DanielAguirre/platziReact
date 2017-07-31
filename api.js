a3a22fe9f1d264e0b818a5cf3a139c5f718c6fa5
Daniel Aguirre <danter1@gmail.com>
2017-07-30 22:50:04 -0500

    Refacto api functions fixing typos and adding page id to the page request, added getcomment fucntion

--- api.js
+++ api.js
@@ -5,13 +5,13 @@ const baseUrl = 'https://jsonplaceholder.typicode.com';
 const api = {
   posts: {
     async getList(page = 1) {
-      const response = await fetch(`${baseUrl}/posts?_page`);
+      const response = await fetch(`${baseUrl}/posts?_page=${page}`);
       const data = await response.json();
       return data;
     },
     async getSingle(id = 1) {
       const response = await fetch(`${baseUrl}/posts/${id}`);
-      const data = await resposne.json();
+      const data = await response.json();
       console.log({data});
      return data;
     },
@@ -24,6 +24,12 @@ const api = {
   users: {
     async getSingle(id = 1){
       const response = await fetch(`${baseUrl}/users/${id}`);
+      const data = await response.json();
+      return data;
+    },
+
+    async getPosts(id = 1){
+      const response = await fetch(`${baseUrl}/posts/?userId=${id}`);
       const data = await response.json();
       return data;
     }
