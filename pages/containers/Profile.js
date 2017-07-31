7d5bd1887c80d26dbdd1139f46a6c25bce186313
Daniel Aguirre <danter1@gmail.com>
2017-07-31 17:39:04 -0500

    Refactor POst to avoid call all the services if already exist user and comment

--- pages/containers/Profile.js
+++ pages/containers/Profile.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Post from '../../posts/containers/post';
+import Loading from '../../shared/components/Loading';
+import api from '../../api';
 
 class Profile extends Component {
+  constructor(props){
+    super(props);
+
+    this.state= {
+      user: {},
+      posts: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount(){
+    const [
+      user,
+      posts
+    ] = await Promise.all([
+      api.users.getSingle(this.props.match.params.id),
+      api.users.getPosts(this.props.match.params.id)
+    ]);
+
+    this.setState({
+      user,
+      posts,
+      loading: false
+    })
+  }
   render() {
+    if(this.state.loading) {
+      return <Loading />
+    }
+
     return (
       <section name="Home">
-        <h1>Post</h1>
-        <Link to="/">Go to Home</Link>
-        <Link to="/random">Go to Random</Link>
+        <h2>Profile of {this.state.user.name}</h2>
+        <fieldset>
+          <legend>Basic info</legend>
+          <input  type="email" value={this.state.user.email} disabled/>
+
+          {this.state.user.address && (
+              <fieldset>
+                <legend>Address</legend>
+                <address>
+                  {this.state.user.address.street}
+                  {this.state.user.address.suite}
+                  {this.state.user.address.city}
+                  {this.state.user.address.zipcode}
+                </address>
+              </fieldset>
+            )}
+          <section>
+            {this.state.posts
+              .map(post => (
+                <Post 
+                key={post.id} 
+                user={this.state.user}
+                {...post} />))}
+          </section>
+        </fieldset>
       </section>
-      )
+    )
   }
 }
 
--- posts/containers/post.js
+++ posts/containers/post.js
@@ -8,30 +8,44 @@ class Post extends Component{
     super(props);
     this.state = {
       loading: true,
-      user:{},
-      comments: [],
+      user: props.user || null,
+      comments: props.comments || null ,
     }
   }
 
   async componentDidMount(){
+    if(!!this.state.user && !!this.state.comments) return this.setState({ loading: false })
     const [
       user,
       comments,
     ] = await Promise.all([
-      api.users.getSingle(this.props.userId),
-      api.posts.getComments(this.props.id)
-    ])
+      !this.state.user ? api.users.getSingle(this.props.userId) : Promise.resolve(null),
+      !this.state.comments ? api.posts.getComments(this.props.id) : Promise.resolve(null),
+    ]);
+
+    this.setState ({
+      loading: false,
+      user: user || this.state.user,
+      comments: comments || this.state.comments,
+    })
   }
 
   render(){
+    console.log('props',this.props)
     return (
       <article id={`post-${this.props.id}`}>
-        <h2>{this.props.title}</h2>
+        <Link to={`/post/${this.props.id}`}>
+          {this.props.title}
+        </Link>
         <p>{this.props.body}</p>
-        {this.state.loading && (
+        {!this.state.loading && (
           <div>
+            <Link to={`/user/${this.state.user.id}`}>
+              {this.state.user.name}
+            </Link>
+            <span>Hey {this.state.comments.length} comentarios</span>
           </div>
-          )}
+        )}
       </article>
     )
   }
